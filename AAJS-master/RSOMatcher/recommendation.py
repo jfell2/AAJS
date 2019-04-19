4166ac6ad0baeb707f8b4890fb6eb1599f74640f
Aashna Wadhwa <aashnaw2@illinois.edu>
2019-04-18 23:41:48 -0500

    updated recommendation system

--- AAJS-master/RSOMatcher/recommendation.py
+++ AAJS-master/RSOMatcher/recommendation.py
@@ -3,11 +3,12 @@ import MySQLdb
 import mysql.connector
 import pandas as pd
 import numpy as np
+import sys
 #app = Flask(__name__)
 
 #@app.route("/test.html")
 
-def mysqlconnect():
+def mysqlconnect(email):
     
     #Trying to connect
     try:
@@ -23,9 +24,9 @@ def mysqlconnect():
 
 # Making Cursor Object For Query Execution
 cursor=db_connection.cursor(buffered=True)
-    
-    # Executing Query
-    cursor.execute("SELECT CURDATE();")
+
+# Executing Query
+cursor.execute("SELECT CURDATE();")
     
     # Above Query Gives Us The Current Date
     # Fetching Data
@@ -34,14 +35,16 @@ cursor=db_connection.cursor(buffered=True)
     # Printing Result Of Above
     #print("Today's Date Is ",m[0])
     
-    df = recommend(db_connection, cursor, "aashnaw2")
+    df = recommend(db_connection, cursor, email)
     
     # Closing Database Connection
     db_connection.close()
+    return df
 
 #return render_template("teest.html", ages=df)
 
-def recommend (db_connection, cursor, NetID):
+def recommend (db_connection, cursor, email):
+    NetID = email[0:email.find('@')]
     
     rso = "SELECT * From RSO"
     rso_df = pd.read_sql(rso, db_connection)
@@ -52,16 +55,31 @@ def recommend (db_connection, cursor, NetID):
     members = "SELECT * From RSO_members"
     members_df = pd.read_sql(members, db_connection)
     
-    
     user_wanted = user_df[user_df['netid']==NetID]
     
-    ass_member = members_df[members_df['netid'] == NetID]
+    user_df['Similarity'] = 0.00000
+    #members_df['Similarity'] = 0.00000
     
-    ass_rsos = pd.DataFrame()
-    
-    for index, row in ass_member.iterrows():
-        #print(row['Title'])
-        ass_rsos = ass_rsos.append(rso_df[rso_df['Title'] == row['Title']])
+    for index in range(user_wanted.shape[0]):
+        for column in user_df.columns[:len(user_df.columns) - 1]:
+            for row in range(user_df.shape[0]):
+                user_df['Similarity'][row] = user_df['Similarity'][row] + levenshtein_ratio_and_distance(str(user_df.iloc[row][column]), str(user_wanted[column].iloc[index]))
+
+#print (user_df)
+best_user = user_df.nlargest(5, 'Similarity')
+if NetID in members_df.values:
+    compare_NetID = best_user.iloc[0]['netid']
+    else:
+        compare_NetID = best_user.iloc[1]['netid']
+#print (compare_NetID)
+
+ass_member = members_df[members_df['netid'] == compare_NetID]
+
+ass_rsos = pd.DataFrame()
+
+for index, row in ass_member.iterrows():
+    #print(row['Title'])
+    ass_rsos = ass_rsos.append(rso_df[rso_df['Title'] == row['Title']])
     
     rso_df['Similarity'] = 0.00000
     for index in range(ass_rsos.shape[0]):
@@ -72,19 +90,20 @@ def recommend (db_connection, cursor, NetID):
                 rso_df['Similarity'][row] = rso_df['Similarity'][row] + levenshtein_ratio_and_distance(rso_df.iloc[row][column], ass_rsos[column].iloc[index])
 
 
-#best_df = rso_df.nlargest(5, 'Similarity')
-best_df = rso_df.sort_values('Similarity', ascending = False).head(5)
-print(best_df)
-#return best_df
+best_df = rso_df.nlargest(5, 'Similarity')
+#best_df = rso_df.sort_values('Similarity', ascending = False).head(5)
+#print(best_df)
+best_df.to_html('filename.html')
+    return best_df
 def levenshtein_ratio_and_distance(s, t, ratio_calc = True):
-    """ levenshtein_ratio_and_distance:
-        Calculates levenshtein distance between two strings.
-        If ratio_calc = True, the function computes the
-        levenshtein distance ratio of similarity between two strings
-        For all i and j, distance[i,j] will contain the Levenshtein
-        distance between the first i characters of s and the
-        first j characters of t
-        """
+    # """ levenshtein_ratio_and_distance:
+    #     Calculates levenshtein distance between two strings.
+    #     If ratio_calc = True, the function computes the
+    #     levenshtein distance ratio of similarity between two strings
+    #     For all i and j, distance[i,j] will contain the Levenshtein
+    #     distance between the first i characters of s and the
+    #     first j characters of t
+    #     """
     # Initialize matrix of zeros
     rows = len(s)+1
     cols = len(t)+1
@@ -110,10 +129,10 @@ def levenshtein_ratio_and_distance(s, t, ratio_calc = True):
             distance[row][col] = min(distance[row-1][col] + 1,      # Cost of deletions
                                      distance[row][col-1] + 1,          # Cost of insertions
                                      distance[row-1][col-1] + cost)     # Cost of substitutions
-if ratio_calc == True:
-    # Computation of the Levenshtein Distance Ratio
-    Ratio = ((len(s)+len(t)) - distance[row][col]) / (len(s)+len(t))
-    #print(Ratio)
+    if ratio_calc == True:
+        # Computation of the Levenshtein Distance Ratio
+        Ratio = ((len(s)+len(t)) - distance[row][col]) / (len(s)+len(t))
+        #print(Ratio)
         return Ratio
 # else:
 #     # print(distance) # Uncomment if you want to see the matrix showing how the algorithm computes the cost of deletions,
@@ -122,6 +141,10 @@ if ratio_calc == True:
 #     return "The strings are {} edits away".format(distance[row][col])
 
 # Function Call For Connecting To Our Database
-mysqlconnect()
+#print(sys.argv[1])
+
+#print ("Hello %s" % who)
+df = mysqlconnect(sys.argv[1])
+print (df)
 # if __name__ == "__main__":
 #     app.run()
